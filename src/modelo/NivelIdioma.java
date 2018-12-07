13a6755b989dea3559da82ca61244a95f6652726
usuario <usuario@pc-dci3-10>
2018-12-07 19:37:37 -0300

    a

--- src/modelo/NivelIdioma.java
+++ src/modelo/NivelIdioma.java
@@ -2,8 +2,18 @@
 package modelo;
 
 public class NivelIdioma {
+    private idioma idiomas;
+    private Nivel nivelIdioma; 
     
     
+    public idioma getidioma(){
+        return idiomas;
+    }
     
-    
+    public void setIdioma (idioma idioma,Nivel nivelIdioma){
+        
+        this.idiomas= idioma;
+        this.nivelIdioma = nivelIdioma;
+    }
+
 }
