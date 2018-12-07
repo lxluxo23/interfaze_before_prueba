87faebb6d6f04d41730d6e6e617e59eeb65fb203
usuario <usuario@pc-dci3-10>
2018-12-07 19:09:04 -0300

    a

---
+++ src/ejecutora/ejecutora.java
@@ -0,0 +1,11 @@
+package ejecutora;
+
+import modelo.Baraja;
+
+public class ejecutora {
+    
+    public static void main(String[] args) {
+        Baraja b = new Baraja();
+    }
+    
+}
--- src/modelo/Baraja.java
+++ src/modelo/Baraja.java
@@ -6,6 +6,20 @@ public class Baraja {
     
     private palo palo;
     
+    public Baraja (palo palo){
+        this.palo= palo;
+        
+    }
     
-    
+    public String getNombrePALO (){
+        
+        
+        String nombre ="";
+        switch(palo){
+            case PICAS:
+                nombre= palo.getNombre();
+        }
+        
+     return nombre;   
+    }  
 }
